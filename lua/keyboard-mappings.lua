88b857ec9da5c7dd495fa82bef5056e69833e828
Bryant <brayanmc.contacto@gmail.com>
2025-03-08 21:50:02 -0400

    new coonfig

--- lua/keyboard-mappings.lua
+++ lua/keyboard-mappings.lua
@@ -8,6 +8,9 @@ vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
 
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-b>', '<C-u>zz')
+
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
@@ -16,6 +19,13 @@ vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 -- Run format on save
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+
 -- Better window swapping
 mapx.nnoremap("<C-h>", "<C-w>h")
 mapx.nnoremap("<C-j>", "<C-w>j")
@@ -23,11 +33,11 @@ mapx.nnoremap("<C-k>", "<C-w>k")
 mapx.nnoremap("<C-l>", "<C-w>l")
 
 -- Utils
-mapx.nnoremap("<C-o>", ":NvimTreeToggle<Cr>")                                     -- Toggle file explorer.
-mapx.nnoremap("<C-p>", ":Telescope find_files<Cr>")                               -- Search for files.
-mapx.nnoremap("<C-s>", ":Lspsaga outline<Cr>")                                    -- Show symbols outline.
-mapx.nnoremap("<C-f>", ":Telescope live_grep<Cr>")                                -- Search in files.
-mapx.nnoremap("<C-D>", ":TroubleToggle<Cr>")                                      -- Show workspace issues.
+mapx.nnoremap("<C-o>", ":NvimTreeToggle<Cr>")       -- Toggle file explorer.
+mapx.nnoremap("<C-p>", ":Telescope find_files<Cr>") -- Search for files.
+-- mapx.nnoremap("<C-s>", ":Lspsaga outline<Cr>")      -- Show symbols outline.
+mapx.nnoremap("<C-p>", ":Telescope live_grep<Cr>")  -- Search in files.
+-- mapx.nnoremap("<C-D>", ":TroubleToggle<Cr>")                                      -- Show workspace issues.
 
 mapx.inoremap("<F5>", "<cmd>:DapContinue<Cr>", "silent", { ft = "go" })           -- Debug go tests in insert mode.
 mapx.nnoremap("<F5>", "<cmd>:DapContinue<Cr>", "silent", { ft = "go" })           -- Debug go tests in normal mode.
